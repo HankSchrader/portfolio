0cd639a53de1e6b9eddf7df5502046395573fc42
Erik Mikac <mikacew@gmail.com>
2019-04-05 23:20:52 -0400

    Working on author site.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutMePanelComponent } from './about-me-panel/about-me-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMePanelComponent
   ],
   imports: [
     BrowserModule
